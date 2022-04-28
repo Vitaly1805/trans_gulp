a66064359a9b09f8f1e99924642fc1f0fe8fda1a
Vitaly1805 <vitalinosov2000@gmail.com>
2022-04-28 23:18:41 +0300

    dgdfgd

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -460,6 +460,19 @@ if(document.querySelector('.typical-work__checkbox')) {
 
 if(document.querySelector('.responsible__table')) {
     let checkboxes = document.querySelectorAll('.responsible-preparation__checkbox');
+    let save = document.getElementById('save-responsibles');
+    let saveInput = document.getElementById('keys-responsibles');
+    let arrResponsiblesId = [];
+
+    save.addEventListener('click', () => {
+        arrResponsiblesId.forEach(e => {
+            if(saveInput.value === '') {
+                saveInput.value = e;
+            } else {
+                saveInput.value = saveInput.value + ' ' + e;
+            }
+        });
+    });
 
     checkboxes.forEach(e => {
         e.parentElement.addEventListener('click', (event) => {
@@ -471,16 +484,35 @@ if(document.querySelector('.responsible__table')) {
                 }
             } 
 
-            if(e.checked) {
-                addRowToChiceTable(e);
-            }
+            toggleRowToChiceTable(e);
         });
     });
 
-    function addRowToChiceTable(elem) {
+    function toggleRowToChiceTable(elem) {
+        let mainTableRow = elem.parentElement.parentElement;
         let choiceTable = document.querySelector('.responsible__table_choice');
-        let col = document.createElement('div');
-        col.innerHTML = 'sdfdsf';
-        choiceTable.appendChild(col);
+        let choiceBlock = document.querySelector('.responsible__choice');
+        let col = mainTableRow.cloneNode(true);
+        let id = col.querySelector('.input').value;
+        let fl = true;
+
+        Array.from(choiceTable.children).forEach(e => {
+            if(e.querySelector('.input') && col.querySelector('.input')) {
+                if(e.querySelector('.input').value === col.querySelector('.input').value) {
+                    let index =  arrResponsiblesId.indexOf(id);
+                    arrResponsiblesId.splice(index, 1);
+                    fl = false;
+                    e.remove();
+                }
+            }
+        });
+
+        if(fl) {
+            arrResponsiblesId.push(id);
+            choiceTable.appendChild(col);
+            choiceBlock.classList.add('responsible__choice_active');
+        } else if(!choiceTable.querySelector('.responsible__row')) {
+            choiceBlock.classList.remove('responsible__choice_active');
+        }
     }
 }
